f4a5456fe9a4e231849c2f02916d722a58602a60
ricban197 <ricardobandeira197@gmail.com>
2022-01-15 08:09:11 -0300

    refazendo

--- JAVA/src/com/dio/MyFirstProgram.java
+++
@@ -1,9 +0,0 @@
-package com.dio;
-import  com.dio.base.Pedido;
-
-public class MyFirstProgram {
-    public static void main(String[] args){
-        final Pedido pedido =new Pedido("code1234");
-        System.out.println(pedido);
-    }
-}
