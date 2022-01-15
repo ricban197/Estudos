7b48986417bfb55618a8607a46f9c0118ec7eb82
ricban197 <ricardobandeira197@gmail.com>
2022-01-15 07:09:58 -0300

    terminado erro n encontra classe principal

--- JAVA/src/com/dio/MyFirstProgram.java
+++ JAVA/src/com/dio/MyFirstProgram.java
@@ -1,9 +1,9 @@
 package com.dio;
 import  com.dio.base.Pedido;
-public class MyFirstProgram {
 
+public class MyFirstProgram {
     public static void main(String[] args){
-        Pedido pedido =new Pedido(code="0001234");
+        final Pedido pedido =new Pedido("code1234");
         System.out.println(pedido);
     }
 }
--- JAVA/src/com/dio/base/Pedido.java
+++ JAVA/src/com/dio/base/Pedido.java
@@ -1,15 +1,17 @@
 package com.dio.base;
+
 public  class Pedido {
-    private  final String code;//codico do pedido
+
+    private final String code;//codico do pedido
 
     public Pedido(String code){
         this.code=code;
     }
-    @Override
-    public String Tostring(){
-    return  "Pedido={"+
-            "code='"+code+"'"+
-            "}";
+    //@Override
+    public String tostring(){
+        return  "Pedido={"+
+                "code='"+code+"'"+
+                "}";
     }
 
 }
